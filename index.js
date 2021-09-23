87f531a3143d29229128021dc03945efa8868777
Avery Lin <tclin100@gmail.com>
2021-09-23 07:57:54 -0400

    commit tree cfec5e70a4ab0e6472349b1e873a75cb1e4cfe97

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0,2)
+}
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2)
+}
+let selectingDrivers= [ returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = i => {
+    return function (fare) {
+        return fare * i
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function (drivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers (drivers)
+}
